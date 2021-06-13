a4e85923b916e80f1e3ab1715d3bdf03ef2442dc
annezdz <annezdz@hotmail.com>
2021-06-13 09:19:26 -0300

    initial commit

---
+++ src/main/java/com/example/api/cars/ApiCarsApplication.java
@@ -0,0 +1,13 @@
+package com.example.api.cars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCarsApplication.class, args);
+	}
+
+}
